fd1ae9eea50f03c2792738abcb3d6097a6c0bc92
MihAlpha <rajaoferisonmihaja@gmail.com>
2025-07-17 09:16:58 +0300

    Sauvegardez tous les modifications

--- frontend/src/components/Message/SuperAdminMessenger.jsx
+++ frontend/src/components/Message/SuperAdminMessenger.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './SuperAdminMessenger.css';
 
 function SuperAdminMessenger() {
   const [agents, setAgents] = useState([]);
